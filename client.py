a6ba789892981221f9caafbcde94b8f1d8948e03
rameshdragon <rameshdragonreddy@gmail.com>
2023-11-25 14:25:15 -0500

    added asyncio connection with peers and added multiple handling statements

--- client.py
+++ client.py
@@ -1,22 +1,92 @@
 import asyncio
+import json
 
-async def connect(self, tracker_address):
-    # connect to server
-    self._tracker_reader, self._tracker_writer = await asyncio.open_connection(*tracker_address)
+async def send_message(reader, writer, message, file_path=None):
+    json_message = json.dumps(message)
+    print(f"Sending JSON: {json_message}")
+    writer.write(json_message.encode())
+
+    await writer.drain()
+
+    if file_path:
+        with open(file_path, 'rb') as file:
+            while True:
+                chunk = file.read(1024)
+                if not chunk:
+                    break
+                writer.write(chunk)
+                await writer.drain()
+        writer.close()
+
+    data = await reader.read(100)
+    response = data.decode()
+    print(f"Received: {response}")
+
+async def main():
+    host = input('Enter IP address: ')
+    port = int(input("Enter Port number: "))
+
+    commands = ["Register", "Login", "Create", "Delete", "Restore", "Exit", "List_Peers"]
+    reader, writer = await asyncio.open_connection(host, port)
+
     try:
-        # send out register message
-        print('Requesting to register')
-        await self._tracker_writer.write("REGISTER")
-        await write_message(self._tracker_writer, {
-            'type': MessageType.REQUEST_REGISTER,
-            'address': self._server_address
-        })
-        message = await read_message(self._tracker_reader)
-        print("message",message)
-    except (ConnectionError, RuntimeError, asyncio.IncompleteReadError):
-        print('Error occurred during communications with tracker.')
-        if not self._tracker_writer.is_closing():
-            self._tracker_writer.close()
-            await self._tracker_writer.wait_closed()
-        raise
-    print('Successfully registered.')
+        while True:
+            # Display available commands
+            print("\nSelect Operation:")
+            for i in range(len(commands)):
+                print("\n{}. {}".format(i + 1, commands[i]))
+
+            # Get user input
+            choice = input("Enter the operation or 'Exit' to quit: ")
+            if choice.lower() == "exit":
+                break
+
+            try:
+                if choice.lower()=="register":
+                    username = input("Enter Username:")
+                    password = input("Password")
+
+                    message = {
+                        "message": "REGISTER",
+                        "username": username,
+                        "password": password
+                    }
+
+                    await send_message(reader, writer, message)
+                
+                elif choice.lower()=="login":
+                    username = input("Enter Username:")
+                    password = input("Password")
+                    
+                    message = {
+                        "message": "LOGIN",
+                        "username": username,
+                        "password": password
+                    }
+
+                    await send_message(reader, writer, message)
+
+                elif choice.lower()=="list_peers":
+                    message = {
+                        "message":"list_peers"
+                    }
+                    await send_message(reader, writer, message)
+                
+                elif choice.lower() == "create":
+                    file_path = input("Enter the path of the file to upload: ")
+                    message = {
+                        "message": "CREATE",
+                        "file_path": file_path
+                    }
+                    await send_message(reader, writer, message, file_path)
+
+            except (ValueError, IndexError):
+                print("Invalid choice. Please enter a valid number or 'Exit' to quit.")
+
+    finally:
+        print("Closing the connection")
+        writer.close()
+        await writer.wait_closed()
+
+if __name__ == "__main__":
+    asyncio.run(main())
