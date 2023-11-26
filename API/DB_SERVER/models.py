06e8d7fe9e934c5ad6977cd2bd89c4a3eb3956a9
Apoorv bansal <apoorv95.ab@gmail.com>
2023-11-25 23:31:22 -0500

    Added routes for addition and deletion of chunks

--- API/DB_SERVER/models.py
+++ API/DB_SERVER/models.py
@@ -1,6 +1,19 @@
 from DB_SERVER import db
+from sqlalchemy.orm import backref
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(40), nullable= False)
+    password = db.Column(db.String(40), nullable=False)
+
+class ChunkFileMappings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(300), unique=True)
+    mappings = db.relationship('ChunkMappings', backref="chunkfilemappings", cascade="all,delete", lazy=True)
+
+class ChunkMappings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chunk_number = db.Column(db.Integer, nullable=False)
+    chunk_peer = db.Column(db.String(100))
+    chunk_id = db.Column(db.String(300))
+    fileMapping_id = db.Column(db.Integer, db.ForeignKey("chunk_file_mappings.id"))
--- API/DB_SERVER/routes.py
+++ API/DB_SERVER/routes.py
@@ -1,6 +1,6 @@
 from DB_SERVER import app, db
-from DB_SERVER.models import Users
-from flask import request 
+from DB_SERVER.models import Users, ChunkMappings, ChunkFileMappings
+from flask import request, jsonify
 
 
 @app.route("/register", methods=['POST'])
@@ -25,4 +25,92 @@ def login():
     if user and user.password==password:
         return "Success"
     else:
-        return "Failed"
+        return "Fail"
+    
+@app.route("/addChunkMapping", methods=["POST"])
+def chunkMapping():
+    try:
+        data = request.json
+        pieces = data["pieces"]
+        filename = data["filename"]
+        mapping_objs = []
+        
+        fileMapping = ChunkFileMappings(filename=filename)
+        db.session.add(fileMapping)
+
+        for piece in pieces:
+            chunk_peer = piece["chunk_peer"]
+            chunk_number = piece["chunk_number"]
+            chunk_id = piece["chunk_id"]
+
+            mapping = ChunkMappings(chunk_number=chunk_number, chunk_id=chunk_id, chunk_peer=chunk_peer, chunkfilemappings=fileMapping)
+            mapping_objs.append(mapping)
+
+        
+        db.session.add_all(mapping_objs)
+        db.session.commit()
+
+        return "Success"
+    except Exception as e:
+        return str(e.args)
+
+@app.route("/retrieveChunks",methods=["GET"])
+def retrieveChunks():
+    try:
+        filename = request.args.get('filename')
+        fileMapping = ChunkFileMappings.query.filter_by(filename=filename).first()
+        if fileMapping:
+            chunks = fileMapping.mappings
+
+            pieces = []
+
+            for i in chunks:
+                pieces.append({
+                    "chunk_peer":i.chunk_peer,
+                    "chunk_number":i.chunk_number,
+                    "chunk_id":i.chunk_id
+                })
+            
+            return jsonify({
+                "status":"success",
+                "chunks":pieces
+            })
+        else:
+            return jsonify({
+                "status":"File not Found"
+            })
+    except Exception as e:
+        return str(e.args)
+
+@app.route("/deleteChunks", methods=["GET"])
+def deleteChunks():
+    try:
+        filename = request.args.get('filename')
+
+        fileMapping = ChunkFileMappings.query.filter_by(filename=filename).first()
+        if fileMapping:
+            chunks = fileMapping.mappings
+
+            pieces = []
+
+            for i in chunks:
+                pieces.append({
+                    "chunk_peer":i.chunk_peer,
+                    "chunk_number":i.chunk_number,
+                    "chunk_id":i.chunk_id
+                })
+            
+            db.session.delete(fileMapping)
+            db.session.commit()
+            
+            return jsonify({
+                "status":"success",
+                "chunks":pieces
+            })
+        
+        else:
+            return jsonify({
+                "status":"File not Found"
+            })
+    except Exception as e:
+        return str(e.args)
